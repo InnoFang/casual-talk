a658e5c727b0777c21d2a811a9602895d886641f
InnoFang <innofang@outlook.com>
2020-02-27 15:04:38 +0800

    :art: update

--- utils/util.go
+++ utils/util.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"html/template"
+	"strings"
 )
 
 func Session(writer http.ResponseWriter, request *http.Request) (sess data.Session, err error) {
@@ -19,6 +20,18 @@ func Session(writer http.ResponseWriter, request *http.Request) (sess data.Sessi
 	return
 }
 
+// parse HTML templates
+// pass in a list of file names, and get a template
+func ParseTemplateFiles(filenames ...string) (t *template.Template) {
+	var files []string
+	t = template.New("layout")
+	for _, file := range filenames {
+		files = append(files, fmt.Sprintf("templates/%s.html", file))
+	}
+	t = template.Must(t.ParseFiles(files...))
+	return
+}
+
 func GenerateHTML(writer http.ResponseWriter, data interface{}, fn ...string) {
 	var files []string
 	for _, file := range fn {
@@ -26,4 +39,10 @@ func GenerateHTML(writer http.ResponseWriter, data interface{}, fn ...string) {
 	}
 	templates := template.Must(template.ParseFiles(files...))
 	templates.ExecuteTemplate(writer, "layout", data)
+}
+
+// convenience function to redirect to the error message page
+func ErrorMessage(writer http.ResponseWriter, request *http.Request, msg string) {
+	url := []string{"/err?msg=", msg}
+	http.Redirect(writer, request, strings.Join(url, ""), 302)
 }
